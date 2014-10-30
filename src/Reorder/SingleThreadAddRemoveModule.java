ce5d9d7dcdbe6c2b49993ee2981421dc45b2c03f
bmistree <bmistree@gmail.com>
2014-10-29 19:15:34 -0700

    Added utility code to generate flow mods.

--- src/Reorder/SingleThreadAddRemoveModule.java
+++ src/Reorder/SingleThreadAddRemoveModule.java
@@ -1,13 +1,30 @@
 package Reorder;
 
-import net.floodlightcontroller.core.IOFSwitch;
+import java.util.List;
+import java.util.ArrayList;
 
+import net.floodlightcontroller.core.IOFSwitch;
+import org.openflow.protocol.OFFlowMod;
 
 public class SingleThreadAddRemoveModule implements IReorderModule
 {
     public final static String REORDER_NAME = "SingleThreadAddRemove";
+    public final static int NUM_ADDS = 10000;
     
     protected SynchronizedSwitch synced_switch = null;
+    protected final List<OFFlowMod> flowmod_list;
+
+    public SingleThreadAddRemoveModule()
+    {
+        flowmod_list = new ArrayList<OFFlowMod>();
+        for (int i =0; i < NUM_ADDS; ++i)
+        {
+            // flow mod add,
+            flowmod_list.add(Util.generate_add_flow_mod(i));
+            // flow mod remove
+            flowmod_list.add(Util.generate_rm_flow_mod(i));
+        }
+    }
     
     /**
        @returns true if got a reordering; false if did not.
--- src/Reorder/Util.java
+++ src/Reorder/Util.java
@@ -1,5 +1,14 @@
 package Reorder;
 
+import java.util.ArrayList;
+
+import net.floodlightcontroller.util.MACAddress;
+
+import org.openflow.protocol.OFOXMFieldType;
+import org.openflow.protocol.instruction.OFInstruction;
+import org.openflow.protocol.OFMatch;
+import org.openflow.protocol.OFFlowMod;
+
 
 public class Util
 {
@@ -8,5 +17,49 @@ public class Util
         System.err.println(reason);
         assert(false);
         System.exit(-1);
+    }
+
+    public static OFFlowMod generate_add_flow_mod(long src_ethernet_addr)
+    {
+        return generate_flow_mod(src_ethernet_addr,true);
+    }
+
+    public static OFFlowMod generate_rm_flow_mod(long src_ethernet_addr)
+    {
+        return generate_flow_mod(src_ethernet_addr,false);
+    }
+
+    /**
+       @returns a flow mod that adds or removes an instruction to
+       perform an action on a particular ethernet address.
+     */
+    protected static OFFlowMod generate_flow_mod(
+        long src_ethernet_addr, boolean is_add)
+    {
+        OFFlowMod to_return = new OFFlowMod();
+        if (is_add)
+            to_return.setCommand(OFFlowMod.OFPFC_ADD);
+        else
+            to_return.setCommand(OFFlowMod.OFPFC_DELETE_STRICT);
+        
+        // generate match
+        OFMatch of_match = generate_ethernet_src_match(src_ethernet_addr);
+        to_return.setMatch(of_match);
+        
+        // add operations for insertions.  empty instruction list
+        // means that we should drop
+        to_return.setInstructions(new ArrayList<OFInstruction>());
+        return to_return;
+    }
+
+    
+    
+    protected static OFMatch generate_ethernet_src_match(
+        long src_ethernet_addr)
+    {
+        MACAddress mac_addr = MACAddress.valueOf(src_ethernet_addr);
+        String ofmatch_comb_str = OFOXMFieldType.ETH_SRC.getName();
+        ofmatch_comb_str += "=" + mac_addr.toString();
+        return OFMatch.fromString(ofmatch_comb_str);
     }
 }
