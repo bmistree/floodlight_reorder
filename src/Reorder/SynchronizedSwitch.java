3f316036a5c9c590d309c0a454a140f61ed04baf
bmistree <bmistree@gmail.com>
2014-11-01 12:58:28 -0700

    Fixed getting number of entries.

--- src/Reorder/SynchronizedSwitch.java
+++ src/Reorder/SynchronizedSwitch.java
@@ -9,6 +9,11 @@ import org.openflow.protocol.OFMessage;
 import org.openflow.protocol.statistics.OFStatistics;
 import org.openflow.protocol.OFStatisticsRequest;
 import org.openflow.protocol.statistics.OFStatisticsType;
+import org.openflow.protocol.OFStatisticsReply;
+import org.openflow.protocol.statistics.OFAggregateStatisticsReply;
+import org.openflow.protocol.statistics.OFAggregateStatisticsRequest;
+import org.openflow.protocol.OFMatch;
+import org.openflow.protocol.OFPort;
 
 import net.floodlightcontroller.core.IOFSwitch;
 import net.floodlightcontroller.core.FloodlightContext;
@@ -36,17 +41,36 @@ public class SynchronizedSwitch
 
     public synchronized int flow_table_entry_size()
     {
-        OFStatisticsRequest stats_request = new OFStatisticsRequest();
-        stats_request.setStatisticsType(OFStatisticsType.AGGREGATE);
+        OFStatisticsRequest req = new OFStatisticsRequest();
+        req.setStatisticsType(OFStatisticsType.AGGREGATE);
+        int requestLength = req.getLengthU();
+        
+        OFAggregateStatisticsRequest specificReq = new OFAggregateStatisticsRequest();
+        OFMatch match = new OFMatch();
+        specificReq.setMatch(match);
+        specificReq.setOutPort(OFPort.OFPP_ANY.getValue());
+        specificReq.setTableId((byte) 0xff);
+        req.setStatistics(specificReq);
+        requestLength += specificReq.getLength();
+        req.setLengthU(requestLength);
 
+        
         int to_return = -1;
         try
         {
             Future<List<OFStatistics>> future_stats_reply_list =
-                of_switch.queryStatistics(stats_request);
+                of_switch.queryStatistics(req);
             List<OFStatistics> stats_reply_list =
                 future_stats_reply_list.get();
-            to_return = stats_reply_list.size();
+
+            to_return = 0;
+            
+            for (OFStatistics stats : stats_reply_list)
+            {
+                OFAggregateStatisticsReply reply =
+                    (OFAggregateStatisticsReply) stats;
+                to_return += reply.getFlowCount();
+            }
         }
         catch(IOException ex)
         {
