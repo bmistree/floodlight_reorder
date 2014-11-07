2065390a294abe2635ae53a3cc1728a0dc4e05a6
bmistree <bmistree@gmail.com>
2014-11-06 21:43:09 -0800

    Trying to build simple main.o.  Doesn't compile because can't link to json.

---
+++ indigo_reorder_test/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <OFStateManager/ofstatemanager.h>
+#include <OFStateManager/ofstatemanager_config.h>
+#include <SocketManager/socketmanager_config.h>
+#include <SocketManager/socketmanager.h>
+
+
+int main (int argc, char** argv)
+{
+    ind_core_config_t core;
+    ind_soc_config_t soc_cfg = { 0 };
+    ind_soc_init(&soc_cfg);
+    
+    /* ind_soc_enable_set(1); */
+    
+    printf("\n\nHello, World!\n\n");
+    return 0;
+}
